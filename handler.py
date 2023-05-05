8dddc89fe0cb2d4e9ea1d91f93fd53de12c87436
ErikBoesen <me@erikboesen.com>
2023-05-05 14:23:23 -0400

    Add starting handler

---
+++ handler.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.insert(0, 'vendor')
+
+import os
+import requests
+import random
+import json
+
+
+def receive(event, context):
+    message = json.loads(event['body'])
+
+    bot_id = message['bot_id']
+    response = process(message)
+    if response:
+        send(response, bot_id)
+
+    return {
+        'statusCode': 200,
+        'body': 'ok'
+    }
+
+
+def process(message):
+    # Prevent self-reply
+    if message['sender_type'] != 'bot':
+        if message['text'].startswith(PREFIX):
+            return None
+
+
+def send(text, bot_id):
+    url = 'https://api.groupme.com/v3/bots/post'
+
+    message = {
+        'bot_id': bot_id,
+        'text': text,
+    }
+    r = requests.post(url, json=message)
